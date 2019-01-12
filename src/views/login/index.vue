4108c102d11828fc38de0855b0bfa882921f72db
wgx <17703253960@163.com>
2019-01-12 21:43:50 +0800

    1

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -43,8 +43,8 @@ export default {
     };
     return {
       loginForm: {
-        username: 'admin',
-        password: 'admin'
+        username: 'test',
+        password: '123456'
       },
       loginRules: {
         username: [{
